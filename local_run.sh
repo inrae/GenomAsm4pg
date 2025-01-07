a9a8a5d99dbeb5c316570559c5e5705637cd17d9
Lucien Piat <lucien.piat@inrae.fr>
2025-01-07 11:35:24 +0100

    adapt the local run file from MSpangepop

--- local_run.sh
+++ local_run.sh
@@ -1,42 +1,55 @@
 #!/bin/bash
 # Script to run locally, DO NOT USE AS IS ON A CLUSTER!
 
-SNG_BIND=$(pwd)
+# Written by Lucien Piat at INRAe
+# 07/01/24
 
-# Get the number of CPU cores dynamically
+SNG_BIND=$(pwd)
 CORES=$(nproc)
 
 run_snakemake() {
+    local option="$1"
 
-    local option="$2"     # The option for dry run or DAG
-    echo "Starting $snakefile..."
-
-    # Execute the Snakemake command with the specified option
-    if [[ "$option" == "dry" ]]; then
-        snakemake --use-singularity --singularity-args "-B $SNG_BIND" -j $CORES -n
-    elif [[ "$option" == "dag" ]]; then
-        snakemake --use-singularity --singularity-args "-B $SNG_BIND" -j $CORES --dag > dag.dot
-        echo "DAG has been generated as dag.png"
-        return
-    else
-        snakemake --use-singularity --singularity-args "-B $SNG_BIND" -j $CORES --forceall
-    fi
+    case "$option" in
+        dry)
+            snakemake --use-singularity --singularity-args "-B $SNG_BIND" -j $CORES -n
+            ;;
+        dag)
+            snakemake --use-singularity --singularity-args "-B $SNG_BIND" -j $CORES --dag > dag.dot
+            if [ $? -eq 0 ]; then
+                echo "DAG has been successfully generated as dag.dot"
+            else
+                echo "Error: Failed to generate DAG."
+                exit 1
+            fi
+            ;;
+        run)
+            snakemake --use-singularity --singularity-args "-B $SNG_BIND" -j $CORES #--forceall
+            ;;
+        *)
+            echo "Invalid option: $option"
+            echo "Usage: $0 [dry|dag|run]"
+            exit 1
+            ;;
+    esac
 
     # Check if the Snakemake command was successful
     if [ $? -eq 0 ]; then
-        echo "$snakefile completed successfully."
+        echo "Snakemake completed successfully."
     else
-        echo "Error: $snakefile failed."
+        echo "Error: Snakemake execution failed."
         exit 1
     fi
 }
 
-if [ $# -eq 0 ]; then
+# Verify arguments
+if [ $# -ne 1 ]; then
     echo "Usage: $0 [dry|dag|run]"
     echo "    dry - run the specified Snakefile in dry-run mode"
     echo "    dag - generate DAG for the specified Snakefile"
-    echo "    run - run the specified Snakefile normally (default)"
+    echo "    run - run the specified Snakefile normally"
     exit 1
 fi
 
-run_snakemake "$option"
+# Execute the function with the provided option
+run_snakemake "$1"
