1d1ceb7df08ef49e5bdba64dccdcc17e3dc0b7b5
Lucien Piat <lucien.piat@inrae.fr>
2025-01-09 09:35:46 +0100

    Add the sample to the log output

--- workflow/scripts/parameter_retrieval.py
+++ workflow/scripts/parameter_retrieval.py
@@ -6,7 +6,7 @@ def get_purge_force(wildcards):
     try :
         force = config["samples"][wildcards.sample]["assembly_purge_force"]
     except KeyError:
-        print('Asm4pg -> No "assembly_purge_force" specified, using l3 by default')
+        print('Asm4pg -> "assembly_purge_force" unspecified for ' + wildcards.sample + ', using l3 by default')
         return '3'
     return force
 
@@ -15,7 +15,7 @@ def get_mode(wildcards):
     try :
         mode = config["samples"][wildcards.sample]["mode"]
     except KeyError:
-        print('Asm4pg -> No "mode" specified, using default assembly mode for hifiasm')
+        print('Asm4pg -> "mode" unspecified for ' + wildcards.sample + ', using default assembly mode for hifiasm')
         return 'default'
     return mode
 
@@ -32,7 +32,7 @@ def get_purge_bool(wildcards):
     try : 
         purge_bool = config["samples"][wildcards.sample]["run_purge_dups"]
     except KeyError:
-        print('Asm4pg -> "run_purge_dups" unspecified, using "False" by default')
+        print('Asm4pg -> "run_purge_dups" unspecified for ' + wildcards.sample + ', using "False" by default')
         return False
     return purge_bool
 
@@ -40,7 +40,7 @@ def get_busco_lin(wildcards) -> str:
     try : 
         lin = config["samples"][wildcards.sample]["busco_lineage"]
     except KeyError:
-        print('Asm4pg -> "busco_lineage" unspecified for ' + config["samples"][wildcards.sample]+ ', using "eukaryota_odb10" by default')
+        print('Asm4pg -> "busco_lineage" unspecified for ' + wildcards.sample + ', using "eukaryota_odb10" by default')
         return "eukaryota_odb10"
     return lin
 
@@ -48,7 +48,7 @@ def get_ploidy(wildcards) -> int:
     try : 
         ploidy = config["samples"][wildcards.sample]["ploidy"]
     except KeyError:
-        print('Asm4pg -> "ploidy" unspecified for ' + config["samples"][wildcards.sample]+ ', using 2 by default')
+        print('Asm4pg -> "ploidy" unspecified for ' + wildcards.sample + ', using 2 by default')
         return 2
     return ploidy
 
@@ -56,6 +56,6 @@ def get_kmer_size(wildcards) -> int:
     try : 
         size = config["samples"][wildcards.sample]["kmer_size"]
     except KeyError:
-        print('Asm4pg -> "kmer_size" unspecified for ' + config["samples"][wildcards.sample]+ ', using 21 by default')
+        print('Asm4pg -> "kmer_size" unspecified for ' + wildcards.sample + ', using 21 by default')
         return 21
     return size
