1b76a3fa125f2f3f7816140294ce174ff0fb9655
Lucien Piat <lucien.piat@inrae.fr>
2025-01-10 10:19:55 +0100

    add conditional ragtag execution

--- workflow/scripts/ragtag_call.sh
+++ workflow/scripts/ragtag_call.sh
@@ -9,22 +9,15 @@ HAP_IN=$2
 HAP_OUT=$3
 REF=$4
 DIRR=$6
-
+THREADS=$7
 
 if [[ "$RAGTAG" == "True" || "$RAGTAG" == "true" ]]; then
-
     echo "Asm4pg -> Running ragtag"
     mkdir -p $DIRR
-    ragtag.py scaffold -o {params.out_dir} -t {threads} {input.reference} {input.assembly} &&
-
+    ragtag.py scaffold -o $DIRR -t $THREADS $REF $HAP_IN
+    mv $DIRR/ragtag.scaffold.fasta $HAP_OUT
+    mv $DIRR/ragtag.scaffold.paf $HAP_OUT
 else
     echo "Asm4pg -> Ragtag option is off"
     mkdir -p $DIRR
-
 fi
-
-
-
-        
-        mv {params.out_dir}/ragtag.scaffold.fasta {output.scaffold} &&
-        mv {params.out_dir}/ragtag.scaffold.paf {output.alignment}
