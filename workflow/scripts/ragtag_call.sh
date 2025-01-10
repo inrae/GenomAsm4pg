373393dbe3e25869ba19ff7f6e8c7df90a333ecb
Lucien Piat <lucien.piat@inrae.fr>
2025-01-10 16:45:09 +0100

    Update the scafolding to output a recap

--- workflow/scripts/ragtag_call.sh
+++ workflow/scripts/ragtag_call.sh
@@ -5,20 +5,33 @@
 # Date: January 6, 2025
 
 RAGTAG=$1
-HAP_IN=$2
-HAP_OUT=$3
+DIRR=$2
+THREADS=$3
 REF=$4
-DIRR=$6
-THREADS=$7
+HAP_IN=$5
+HAP_OUT=$6
+RECAP=$7
+
+# Echo parameters into the recap file
+echo "RAGTAG: $RAGTAG" > $RECAP
+echo "DIRR: $DIRR" >> $RECAP
+echo "THREADS: $THREADS" >> $RECAP
+echo "REF: $REF" >> $RECAP
+echo "HAP_IN: $HAP_IN" >> $RECAP
+echo "HAP_OUT: $HAP_OUT" >> $RECAP
 
 if [[ "$RAGTAG" == "True" || "$RAGTAG" == "true" ]]; then
     echo "Asm4pg -> Running ragtag"
+    echo "Ragtag execution started" >> $RECAP
     mkdir -p $DIRR
+
     ragtag.py scaffold -o $DIRR -t $THREADS $REF $HAP_IN
     gzip $DIRR/ragtag.scaffold.fasta
     mv $DIRR/ragtag.scaffold.fasta.gz $HAP_OUT
-    mv $DIRR/ragtag.scaffold.paf $HAP_OUT
+    echo "Ragtag execution completed" >> $RECAP
+    echo "Output file: $HAP_OUT" >> $RECAP
 else
     echo "Asm4pg -> Ragtag option is off"
+    echo "Ragtag option is off" >> $RECAP
     mkdir -p $DIRR
 fi
