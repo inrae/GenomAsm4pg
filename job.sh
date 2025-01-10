841205bd7dabea1bca1cc5a5be8f29e1ddce2a3b
Lucien Piat <lucien.piat@inrae.fr>
2025-01-10 10:20:53 +0100

    Adapt job.sh to new workflow

--- job.sh
+++ job.sh
@@ -1,76 +1,65 @@
 #!/bin/bash
-################################ Slurm options #################################
-### prepare_calling_jobs
-#SBATCH -J smk_main
-### Max run time "hours:minutes:seconds"
-#SBATCH --time=96:00:00
-#SBATCH --ntasks=1 #nb of processes
-#SBATCH --cpus-per-task=1 # nb of cores for each process(1 process)
-#SBATCH --mem=10G # max of memory (-m) 
-### Requirements nodes/servers (default: 1)
-#SBATCH --nodes=1
-### Requirements cpu/core/task (default: 1)
-#SBATCH --ntasks-per-node=1
-#SBATCH -o slurm_logs/snakemake.%N.%j.out
-#SBATCH -e slurm_logs/snakemake.%N.%j.err
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=lucien.piat@inare.fr
-################################################################################
+#SBATCH --cpus-per-task=1
+#SBATCH -o slurm_logs/out_job_%j.out
+#SBATCH -e slurm_logs/err_job_%j.err
+#SBATCH --time=80:00:00
+#SBATCH -J asm4pg
+#SBATCH --mem=10G
 
-# Useful information to print
-echo '########################################'
-echo 'Date:' $(date --iso-8601=seconds)
-echo 'User:' $USER
-echo 'Host:' $HOSTNAME
-echo 'Job Name:' $SLURM_JOB_NAME
-echo 'Job ID:' $SLURM_JOB_ID
-echo 'Number of nodes assigned to job:' $SLURM_JOB_NUM_NODES
-echo 'Total number of cores for job (?):' $SLURM_NTASKS
-echo 'Number of requested cores per node:' $SLURM_NTASKS_PER_NODE
-echo 'Nodes assigned to job:' $SLURM_JOB_NODELIST
-echo 'Number of CPUs assigned for each task:' $SLURM_CPUS_PER_TASK
-echo 'Directory:' $(pwd)
-# Detail Information:
-echo 'scontrol show job:'
-scontrol show job $SLURM_JOB_ID
-echo '########################################'
+# Verify arguments
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 [dry|dag|run]"
+    echo "    dry - run the specified Snakefile in dry-run mode"
+    echo "    dag - generate DAG for the specified Snakefile"
+    echo "    run - run the specified Snakefile normally"
+    exit 1
+fi
 
-# Function to load modules
-load_modules() {
-    module purge  # Clear any previously loaded modules
+# Update this with the path to your images
+echo 'Loading modules'
+module purge
+module load containers/Apptainer/1.2.5 
+module load devel/Miniconda/Miniconda3
 
-    # Loop through each module and load it
-    for module_name in "$@"; do
-        module load "$module_name"
-    done
-}
-
-# Here specify the modules to load and their path
-load_modules "python/3.9.7" "snakemake/6.5.1"
-
-### variables
-SNG_BIND="/mnt/cbib/pangenoak_trials/GenomAsm4pg/"
-CLUSTER_CONFIG=".config/snakemake_profile/slurm/cluster_config.yml"
-MAX_CORES=10
-PROFILE=".config/snakemake_profile/slurm"
+echo 'Activating environment'
+source activate wf_env
 
 echo 'Starting Snakemake workflow'
 
-### Snakemake commands
-if [ "$1" = "dry" ]
-then
-    # dry run
-    snakemake --profile $PROFILE -j $MAX_CORES --use-singularity --singularity-args "-B $SNG_BIND" --cluster-config $CLUSTER_CONFIG -n -r
-elif [ "$1" = "dag" ]
-then
-    # generate DAG
-    snakemake --profile $PROFILE -j $MAX_CORES --use-singularity --singularity-args "-B $SNG_BIND" --cluster-config $CLUSTER_CONFIG --dag > dag.dot
-    echo "DAG has been generated as dag.png"
-elif [ -z "$1" ]
-then
-    # run
-    snakemake --profile $PROFILE -j $MAX_CORES --use-singularity --singularity-args "-B $SNG_BIND" --cluster-config $CLUSTER_CONFIG
-else
-    echo "Error: Invalid argument. Use 'dry', 'dag', or no argument." >&2
-    exit 1
-fi
+run_snakemake() {
+    local option="$1"
+
+    case "$option" in
+        dry)
+            snakemake -s "$snakefile" -c $(nproc) --dry-run
+            ;;
+        dag)
+            snakemake -c $(nproc) --dag > dag.dot
+            if [ $? -eq 0 ]; then
+                echo "Asm4pg -> DAG has been successfully generated as dag.dot"
+            else
+                echo "Asm4pg -> Error: Failed to generate DAG."
+                exit 1
+            fi
+            ;;
+        run)
+            snakemake --workflow-profile ./.config/snakemake/profiles/slurm
+            ;;
+        *)
+            echo "Invalid option: $option"
+            echo "Usage: $0 [dry|dag|run]"
+            exit 1
+            ;;
+    esac
+
+    # Check if the Snakemake command was successful
+    if [ $? -eq 0 ]; then
+        echo "Asm4pg -> Snakemake workflow completed successfully."
+    else
+        echo "Asm4pg -> Error: Snakemake workflow execution failed."
+        exit 1
+    fi
+}
+
+# Execute the function with the provided option
+run_snakemake "$1"
