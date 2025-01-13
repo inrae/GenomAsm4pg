5e3840c7e0fd5c752e4b0e4b6076d91b860e2ea1
Lucien Piat <lucien.piat@inrae.fr>
2025-01-13 15:22:17 +0100

    Add error handling and verbose

--- workflow/scripts/ragtag_call.sh
+++ workflow/scripts/ragtag_call.sh
@@ -22,16 +22,20 @@ echo "HAP_OUT: $HAP_OUT" >> $RECAP
 
 if [[ "$RAGTAG" == "True" || "$RAGTAG" == "true" ]]; then
     echo "Asm4pg -> Running ragtag"
-    echo "Ragtag execution started" >> $RECAP
+    echo "Asm4pg -> Ragtag execution started" >> $RECAP
     mkdir -p $DIRR
 
-    ragtag.py scaffold -o $DIRR -t $THREADS $REF $HAP_IN
-    gzip $DIRR/ragtag.scaffold.fasta
-    mv $DIRR/ragtag.scaffold.fasta.gz $HAP_OUT
-    echo "Ragtag execution completed" >> $RECAP
-    echo "Output file: $HAP_OUT" >> $RECAP
+    if ragtag.py scaffold -o $DIRR -t $THREADS $REF $HAP_IN; then
+        gzip $DIRR/ragtag.scaffold.fasta
+        mv $DIRR/ragtag.scaffold.fasta.gz $HAP_OUT
+        echo "Asm4pg -> Ragtag execution completed" >> $RECAP
+        echo "Output file: $HAP_OUT" >> $RECAP
+    else
+        echo "Asm4pg -> Ragtag execution failed" >> $RECAP
+        exit 1
+    fi
 else
     echo "Asm4pg -> Ragtag option is off"
-    echo "Ragtag option is off" >> $RECAP
+    echo "Asm4pg -> Ragtag option is off" >> $RECAP
     mkdir -p $DIRR
 fi
