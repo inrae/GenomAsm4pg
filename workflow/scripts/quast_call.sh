4361afae934d4e45d72a576a60d8af448e8ed224
Lucien Piat <lucien.piat@inrae.fr>
2025-01-16 11:44:01 +0100

    Include quast plots in report

--- workflow/scripts/quast_call.sh
+++ workflow/scripts/quast_call.sh
@@ -7,15 +7,12 @@
 # Arguments
 REFERENCE_GENOME="$1"
 PURGE_BOOL="$2"
-RAGTAG_BOOL="$3"
-RAW_HAP1="$4"
-RAW_HAP2="$5"
-FINAL_HAP1="$6"
-FINAL_HAP2="$7"
-RAGTAG_HAP1="$8"
-RAGTAG_HAP2="$9"
-OUTPUT_DIR="${10}"
-RESULT_DIR="$11"
+RAW_HAP1="$3"
+RAW_HAP2="$4"
+FINAL_HAP1="$5"
+FINAL_HAP2="$6"
+OUTPUT_DIR="${7}"
+RESULT_DIR="$8"
 
 # Create the list of genomes to run quast on
 echo "Asm4pg -> Preparing genome list for QUAST analysis..."
@@ -27,14 +24,9 @@ if [ "$PURGE_BOOL" == "True" ]; then
     echo " - Purge option enabled: added raw haplotypes: $RAW_HAP1, $RAW_HAP2"
 fi
 
-if [ "$RAGTAG_BOOL" == "True" ]; then
-    genomes+=("$RAGTAG_HAP1" "$RAGTAG_HAP2")
-    echo " - RagTag option enabled: added RagTag haplotypes: $RAGTAG_HAP1, $RAGTAG_HAP2"
-fi
-
 # Build the quast command
 echo "Asm4pg -> Building the QUAST command..."
-quast_cmd="python /quast-5.2.0/metaquast.py --threads 20 --large --no-html --no-check --plots-format png "
+quast_cmd="python /quast-5.2.0/metaquast.py --threads 20 --large --no-check --no-snps --no-icarus --plots-format png "
 if [ "$REFERENCE_GENOME" != "None" ]; then
     echo " - Reference genome specified: $REFERENCE_GENOME"
     quast_cmd+="--reference $REFERENCE_GENOME "
@@ -49,13 +41,6 @@ echo "$quast_cmd"
 # Run the quast command
 echo "Asm4pg -> Running QUAST..."
 eval $quast_cmd
-
-# Isolating desired outputs
-echo "Asm4pg -> Isolating QUAST basic stat plots..."
-mkdir -p "$RESULT_DIR"
-cp "$OUTPUT_DIR/combined_reference/basic_stats/cumulative_plot.png" "$RESULT_DIR/cumulative_plot.png"
-cp "$OUTPUT_DIR/combined_reference/basic_stats/GC_content_plot.png" "$RESULT_DIR/GC_content_plot.png"
-cp "$OUTPUT_DIR/combined_reference/basic_stats/Nx_plot.png" "$RESULT_DIR/Nx_plot.png"
 
 # Exit status check
 if [ $? -eq 0 ]; then
