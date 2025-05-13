c472e4906534ad0f287e51d46d148592405d5674
Lucien Piat <lpiat@bb8-1.cluster>
2025-05-13 14:14:44 +0200

    add option to preserve fastq format

--- workflow/scripts/input_conversion.sh
+++ workflow/scripts/input_conversion.sh
@@ -6,14 +6,10 @@
 set -e  # Exit on error
 
 INPUT_FILE="$1"
-OUTPUT_FILE="$2"
+OUTPUT_FA="$2"
 THREADS="$3"
-
-# Check if required arguments are provided
-if [[ -z "$INPUT_FILE" || -z "$OUTPUT_FILE" || -z "$THREADS" ]]; then
-    echo "Usage: $0 <input_file> <output_file> <threads>"
-    exit 1
-fi
+MODE="$4"
+OUTPUT_FQ="$5"
 
 # Get file extension after the first dot
 EXTENSION="${INPUT_FILE#*.}"  # Remove everything before the first dot
@@ -21,31 +17,55 @@ EXTENSION="${EXTENSION,,}"     # Convert to lowercase
 
 echo "🔹 Asm4pg -> Checking input file type: $INPUT_FILE"
 
+if [[ "$MODE" == "ont" ]]; then
+    echo "🔹 Asm4pg -> ONT mode detected, producing a FASTQ (assembly) file and FASTA file (QC)"
+fi
+
 # Handle different file extensions
 case "$EXTENSION" in
     fa.gz|fasta.gz)
+        if [[ "$MODE" == "ont" ]]; then
+            echo "❌ Asm4pg -> In ONT mode, FASTQ input is required"
+            exit 1
+        fi
         echo "🔹 Asm4pg -> No conversion needed for .fasta.gz or .fa.gz, copying"
-        cp "$INPUT_FILE" "$OUTPUT_FILE"
+        cp "$INPUT_FILE" "$OUTPUT_FA"
         ;;
 
     fq.gz|fastq.gz)
         echo "🔹 Asm4pg -> Converting FastQ to Fasta (gzipped)"
-        zcat "$INPUT_FILE" | awk 'NR%4==1{sub(":.*", "", $0); print ">" substr($0,2)} NR%4==2{print}' | pigz -p "$THREADS" > "$OUTPUT_FILE"
+        zcat "$INPUT_FILE" | awk 'NR%4==1{sub(":.*", "", $0); print ">" substr($0,2)} NR%4==2{print}' | pigz -p "$THREADS" > "$OUTPUT_FA"
+        if [[ "$MODE" == "ont" ]]; then
+            echo "🔹 Asm4pg -> Copying FastQ input to output for ONT mode"
+            cp "$INPUT_FILE" "$OUTPUT_FQ"
+        fi
         ;;
 
     fasta|fa)
+        if [[ "$MODE" == "ont" ]]; then
+            echo "❌ Asm4pg -> In ONT mode, FASTQ input is required"
+            exit 1
+        fi
         echo "🔹 Asm4pg -> Converting and compressing .fasta or .fa file"
-        awk 'NR%2==1{sub(":.*", "", $0); print ">" substr($0,2)} NR%2==2{print}' "$INPUT_FILE" | pigz -p "$THREADS" > "$OUTPUT_FILE"
+        awk 'NR%2==1{sub(":.*", "", $0); print ">" substr($0,2)} NR%2==2{print}' "$INPUT_FILE" | pigz -p "$THREADS" > "$OUTPUT_FA"
         ;;
 
     fastq|fq)
         echo "🔹 Asm4pg -> Converting and compressing .fastq or .fq file"
-        awk 'NR%4==1{sub(":.*", "", $0); print ">" substr($0,2)} NR%4==2{print}' "$INPUT_FILE" | pigz -p "$THREADS" > "$OUTPUT_FILE"
+        awk 'NR%4==1{sub(":.*", "", $0); print ">" substr($0,2)} NR%4==2{print}' "$INPUT_FILE" | pigz -p "$THREADS" > "$OUTPUT_FA"
+        if [[ "$MODE" == "ont" ]]; then
+            echo "🔹 Asm4pg -> Compressing FastQ input for ONT mode"
+            pigz -p "$THREADS" -c "$INPUT_FILE" > "$OUTPUT_FQ"
+        fi
         ;;
 
     bam)
         echo "🔹 Asm4pg -> Converting BAM to FASTA"
-        samtools fasta -@ "$THREADS" "$INPUT_FILE" | pigz -p "$THREADS" > "$OUTPUT_FILE"
+        samtools fasta -@ "$THREADS" "$INPUT_FILE" | pigz -p "$THREADS" > "$OUTPUT_FA"
+        if [[ "$MODE" == "ont" ]]; then
+            echo "🔹 Asm4pg -> Converting BAM to FASTQ for ONT mode"
+            samtools fastq -@ "$THREADS" "$INPUT_FILE" | pigz -p "$THREADS" > "$OUTPUT_FQ"
+        fi
         ;;
 
     *)
@@ -54,4 +74,9 @@ case "$EXTENSION" in
         ;;
 esac
 
-echo "✅ Asm4pg -> Processing completed: $OUTPUT_FILE"
+if [[ "$MODE" != "ont" ]]; then
+    echo "🔹 Asm4pg -> ont mode off, no need for fastq file"
+else
+    echo "🔹 Asm4pg -> Produced FASTA.gz and FASTQ.gz files"
+fi
+echo "✅ Asm4pg -> Processing completed: $OUTPUT_FA"
